9e094f1c54a7311d14cfcd32765e8618bac3c1dd
Marlowe <f15534666671@163.com>
2019-05-26 20:22:34 +0800

    add 7.10 - 8.2.5

---
+++ C++_course/函数探幽/filefunc.cpp
@@ -0,0 +1,16 @@
+// filefunc.cpp -- function with ostream & parameter
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+void file_it(ostream & os, double fo, const double fe[], int n);
+const int LIMIT = 5;
+
+int main()
+{
+    ofstream fout;
+    const char * fn = "ep-data.txt";
+    fout
+}
