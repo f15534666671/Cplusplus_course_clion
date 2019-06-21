fd3d0541e7321f0f90aed0137c0c818c64ce3ae1
sanmu <f15534666671@163.com>
2019-06-21 13:24:47 +0800

    Until 13/acctabc commit and backup

---
+++ C++_course/13类继承/tabtenn0/tabtenn0.h
@@ -0,0 +1,24 @@
+// Created by sanmu on 19-6-20.
+// tabtenn0.h -- a table-tennis base class
+
+#ifndef TABTENN0_H
+#define TABTENN0_H
+
+#include <string>
+
+using std::string;
+class TableTennisPlayer
+{
+private:
+    string firstname;
+    string lastname;
+    bool hasTable;
+public:
+    TableTennisPlayer (const string & fn = "none",
+                        const string & ln = "none", bool ht = false);
+    void Name() const;
+    bool HasTable() const { return hasTable; };
+    void ResetTable(bool v) { hasTable = v; };
+};
+
+#endif
